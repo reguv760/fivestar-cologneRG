124b08ff3938a852d3e974184420d4fd96825cad
reguv760 <reguv760@gmail.com>
2018-10-10 10:59:52 -0700

    apollo return query

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
+
+//styles
 import './App.scss';
+
+//graphql
+import { Query } from 'react-apollo';
+import { GET_ALL_COLOGNES } from './../queries';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Home</h1>
+
+        <Query query={GET_ALL_COLOGNES}>
+          {({ data, loading, error }) => {
+            if (loading) return <div>Loading...</div>;
+            if (error) return <div>Error</div>;
+            console.log(data);
+
+            return <p>Colognes</p>;
+          }}
+        </Query>
       </div>
     );
   }
