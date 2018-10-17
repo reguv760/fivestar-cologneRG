b9045154e6d46a0da2b356cea11c63e744a1c71c
reguv760 <reguv760@gmail.com>
2018-10-17 11:26:48 -0700

    router component added, signin + signup pages added

---
+++ client/src/components/Auth/Signup.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class Signup extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h2 className="App">Signup</h2>
+        <form className="form">
+          <input type="text" name="username" placeholder="Username" />
+          <input type="email" name="email" placeholder="Email Address" />
+          <input type="password" name="password" placeholder="Password" />
+          <input
+            type="password"
+            name="passwordConfirmation"
+            placeholder="Confirm Password"
+          />
+          <button type="submit" className="button-primary">
+            Submit
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Signup;
