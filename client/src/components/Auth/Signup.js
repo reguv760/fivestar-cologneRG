510b1ddf2abb711c28bb70b66b6183ab555843c9
reguv760 <reguv760@gmail.com>
2018-10-24 11:14:41 -0700

    fixed eslint + prettier V2

--- client/src/components/Auth/Signup.js
+++ client/src/components/Auth/Signup.js
@@ -1,23 +1,82 @@
 import React, { Component } from "react";
 
 class Signup extends Component {
+  state = {
+    username: "",
+    email: "",
+    password: "",
+    passwordConfirmation: ""
+  };
+
+  handleChange = event => {
+    const { name, value } = event.target;
+    // console.log(name, ':', value);
+
+    // declare state variables to init:::
+    // /modify state variables using setState:::
+    this.setState({
+      [name]: value
+    });
+  };
+
   render() {
+    // destructuring variables:::
+    const { username, email, password, passwordConfirmation } = this.state;
     return (
       <div className="App">
-        <h2 className="App">Signup</h2>
+        <h2 className="App"> Signup </h2>{" "}
         <form className="form">
-          <input type="text" name="username" placeholder="Username" />
-          <input type="email" name="email" placeholder="Email Address" />
-          <input type="password" name="password" placeholder="Password" />
-          <input
-            type="password"
-            name="passwordConfirmation"
-            placeholder="Confirm Password"
-          />
-          <button type="submit" className="button-primary">
-            Submit
-          </button>
-        </form>
+          <label htmlFor="username">
+            <input
+              type="text"
+              name="username"
+              id="username"
+              placeholder="Username"
+              onChange={this.handleChange}
+              value={username}
+            />
+            Username{" "}
+          </label>{" "}
+          <label htmlFor="email">
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Email"
+              onChange={this.handleChange}
+              value={email}
+            />
+            Email{" "}
+          </label>{" "}
+          <label htmlFor="password">
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Password"
+              onChange={this.handleChange}
+              value={password}
+            />
+            Password{" "}
+          </label>{" "}
+          <label htmlFor="passwordConfirmation">
+            <input
+              type="password"
+              name="passwordConfirmation"
+              id="passwordConfirmation"
+              placeholder="Confirm Password"
+              onChange={this.handleChange}
+              value={passwordConfirmation}
+            />
+            Confirm Password{" "}
+          </label>{" "}
+          <div>
+            <button type="button" className="button-primary">
+              {" "}
+              Signup{" "}
+            </button>{" "}
+          </div>{" "}
+        </form>{" "}
       </div>
     );
   }
