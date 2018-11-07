a5d5e6283e838c9f4650f387d1c3bef9e1d4d401
reguv760 <reguv760@gmail.com>
2018-11-07 11:16:39 -0800

    add serverside signin graphql

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,8 +1,18 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 const createToken = (user, secret, expiresIn) => {
   const { username, email } = user;
-  return jwt.sign({ username, email }, secret, { expiresIn });
+  return jwt.sign(
+    {
+      username,
+      email
+    },
+    secret,
+    {
+      expiresIn
+    }
+  );
 };
 
 exports.resolvers = {
@@ -10,7 +20,7 @@ exports.resolvers = {
     getAllColognes: async (root, args, { Cologne }) => {
       const allColognes = await Cologne.find();
       return allColognes;
-    },
+    }
   },
 
   Mutation: {
@@ -23,25 +33,50 @@ exports.resolvers = {
         scentName,
         scentPrice,
         description,
-        username,
+        username
       }).save();
 
       return newCologne;
     },
 
+    signinUser: async (root, { username, password }, { User }) => {
+      const user = await User.findOne({
+        username
+      });
+      if (!user) {
+        throw new Error("User not found");
+      }
+      // check to make sure password matches with user
+      // that is found
+      const isValidPassword = await bcrypt.compare(password, user.password);
+      if (!isValidPassword) {
+        throw new Error("Invalid Password");
+      }
+      // all good? return token
+      return {
+        token: createToken(user, process.env.SECRET, "1hr")
+      };
+    },
+
     signupUser: async (root, { username, email, password }, { User }) => {
       // check if user already exists
-      const user = await User.findOne({ username });
+      const user = await User.findOne({
+        username
+      });
       if (user) {
-        throw new Error('User already exists');
+        throw new Error("User already exists");
       }
       // user doesn't exist, create one
       const newUser = await new User({
         username,
         email,
-        password,
+        password
       }).save();
-      return { token: createToken(newUser, process.env.SECRET, '1hr') };
-    },
-  },
+      return {
+        token: createToken(newUser, process.env.SECRET, "1hr")
+      };
+    }
+  }
 };
+
+// signin function"""
